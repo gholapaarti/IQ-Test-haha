a7a541aeaaab717207da88d7bee46127db208e14
gholapaarti <gholapaarti0@gmail.com>
2026-07-28 22:11:44 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,87 @@
+questions = [
+    ["What is the capital of India?",
+     ["Mumbai", "New Delhi", "Kolkata", "Chennai"], 2],
+
+    ["How many states does India have?",
+     ["26", "27", "28", "29"], 3],
+
+    ["Who is known as the Father of the Indian Constitution?",
+     ["Mahatma Gandhi", "B. R. Ambedkar", "Jawaharlal Nehru", "Sardar Patel"], 2],
+
+    ["Which is India's national animal?",
+     ["Lion", "Tiger", "Elephant", "Leopard"], 2],
+
+    ["Which is the longest river in India?",
+     ["Yamuna", "Godavari", "Ganga", "Krishna"], 3],
+
+    ["Which planet is called the Red Planet?",
+     ["Earth", "Mars", "Venus", "Jupiter"], 2],
+
+    ["How many colors are there in the Indian flag?",
+     ["2", "3", "4", "5"], 2],
+
+    ["Who wrote the Indian National Anthem?",
+     ["Bankim Chandra", "Rabindranath Tagore", "Premchand", "Vivekananda"], 2],
+
+    ["Which city is known as the Silicon Valley of India?",
+     ["Pune", "Hyderabad", "Bengaluru", "Chennai"], 3],
+
+    ["Which is the largest state in India by area?",
+     ["Maharashtra", "Rajasthan", "Gujarat", "Madhya Pradesh"], 2],
+
+    ["Which symbol is used for comments in Python?",
+     ["//", "#", "<!--", "/*"], 2],
+
+    ["Which keyword is used to define a function?",
+     ["func", "function", "def", "define"], 3],
+
+    ["Which data type is mutable?",
+     ["tuple", "string", "list", "int"], 3],
+
+    ["What does len() return?",
+     ["Sum", "Length", "Average", "Maximum"], 2],
+
+    ["Which loop repeats until a condition becomes False?",
+     ["if", "while", "switch", "foreach"], 2],
+
+    ["What is the output of print(type(5))?",
+     ["float", "int", "string", "bool"], 2],
+
+    ["Which keyword exits a loop?",
+     ["stop", "break", "exit", "end"], 2],
+
+    ["Which function takes user input?",
+     ["print()", "input()", "scan()", "read()"], 2],
+
+    ["Which symbol is used for string formatting (f-string)?",
+     ["f", "$", "@", "%"], 1],
+
+    ["Which number comes next? 2, 4, 8, 16, ?",
+     ["20", "24", "32", "30"], 3],
+
+    ["Odd one out?",
+     ["Apple", "Mango", "Carrot", "Banana"], 3],
+
+    ["Which is heavier?",
+     ["1 kg Iron", "1 kg Cotton", "Both Equal", "Cannot Say"], 3],
+
+    ["Complete the pattern: A, C, E, G, ?",
+     ["H", "I", "J", "K"], 2],
+
+    ["5 + 3 × 2 = ?",
+     ["16", "11", "13", "10"], 2],
+
+    ["Find the missing number: 1, 4, 9, 16, ?",
+     ["20", "21", "25", "36"], 3],
+
+    ["Which month has 28 days?",
+     ["February", "January", "All Months", "December"], 3],
+
+    ["If TODAY is Monday, what day comes after 10 days?",
+     ["Wednesday", "Thursday", "Friday", "Saturday"], 2],
+
+    ["Which number is odd?",
+     ["18", "24", "31", "42"], 3]
+]
+
+ 
